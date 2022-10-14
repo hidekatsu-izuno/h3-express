7ad9b886c558808e546f46b32ff805dfe5adc5b4
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2022-10-15 02:20:05 +0900

    Improve code

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# h3-extras
+# h3-express
+
+Express polyfill for h3
+
+## define a handler
+
+```typescript
+import { defineExpressHandler } from 'h3-express'
+
+export default defineExpressHandler((req, res) => {
+  req.json({
+    req.path
+  })
+})
+```
+
+## define a middleware
+
+```typescript
+import { defineExpressHandler } from 'h3-express'
+import session from 'express-session'
+
+export default defineExpressHandler(session({
+  ...
+}))
+```
+
+## define a compound middleware
+
+```typescript
+import { defineExpressHandler } from 'h3-express'
+import express from 'express'
+import session from 'express-session'
+import fileupload from 'express-fileupload'
+
+export default defineExpressHandler(new express.Router()
+  .use(session({
+    ...
+  }))
+  .use(fileupload({
+    ...
+  }))
+)
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,50 @@
-export * from './express'
+import { IncomingMessage, ServerResponse } from 'node:http'
+import express, { Request, Response } from 'express'
+
+declare type Handler = (req: Request, res: Response, next?: (err?: Error) => any) => any
+
+export function defineExpressHandler(handler: Handler) {
+  if (handler.length === 2) {
+    return (req: IncomingMessage, res: ServerResponse) => {
+      const ereq = toExpressRequest(req)
+      const eres = toExpressResponse(res)
+      return handler(ereq, eres)
+    }
+  } else {
+    return (req: IncomingMessage, res: ServerResponse, next: (err?: Error) => any) => {
+      const ereq = toExpressRequest(req)
+      const eres = toExpressResponse(res)
+      return handler(ereq, eres, next as any)
+    }
+  }
+}
+
+function toExpressRequest(req: any): Request {
+  const descs = Object.getOwnPropertyDescriptors(express.request)
+  for (const key in descs) {
+      Object.defineProperty(req, key, descs[key])
+  }
+  return req as any
+}
+
+function toExpressResponse(res: any): Response {
+  const descs = Object.getOwnPropertyDescriptors(express.response)
+  for (const key in descs) {
+      Object.defineProperty(res, key, descs[key])
+  }
+
+  // Nuxt 3 bug: https://github.com/nuxt/framework/issues/3623
+  const _setHeader = res.setHeader;
+  res.setHeader = function setHeader(...args: any[]) {
+      if (!res.headersSent) {
+          _setHeader.apply(res, args)
+      }
+      return res
+  }
+
+  res._implicitHeader = function () {
+      res.writeHead(res.statusCode)
+  }
+  return res as any
+}
+
